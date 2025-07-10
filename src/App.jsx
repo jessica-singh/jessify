d35790310cb531cd958f279e96d207ee7bdf01e7
jessica-singh <jessicaasinghh12@gmail.com>
2025-07-10 15:16:06 +0530

    connecting front w backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
-// App.jsx (Updated to support PrivateRoute)
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-
-const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/login" />;
-};
 
 const App = () => {
   return (
-    <div className="pt-20">
+    
+    <div className="pt-20"> {/* padding top so content clears navbar */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        } />
       </Routes>
     </div>
   );
